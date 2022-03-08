0ee1900cf2bd5b4873d9e078f9e3b86b08bfe617
YHY <yhy@YHYdeMacBook-Pro.local>
2022-03-08 09:33:44 -0500

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,4 @@
 const path = require("path");
-const fs = require('fs');
-
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-
-const secrets = JSON.parse(
-    fs.readFileSync(".secrets").toString().trim()
-);
 
 module.exports = {
 
@@ -15,21 +8,6 @@ module.exports = {
             host: "localhost",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
             network_id: 1337,    // Any network (default: none)
-        },
-        ropsten: {
-            provider: () => new HDWalletProvider(secrets.seed, `https://ropsten.infura.io/v3/${secrets.projectId}`),
-            network_id: 3,       // Ropsten's id
-            gas: 5500000,        // Ropsten has a lower block limit than mainnet
-            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        },
-        private: {
-            host: "47.94.20.232",     // Localhost (default: none)
-            port: 8545,            // Standard Ethereum port (default: none)
-            provider: () => new HDWalletProvider(secrets.mnemonic,"http://47.94.20.232:8545"),
-            network_id: 3458,   // This network is yours, in the cloud.
-            production: true    // Treats this network as if it was a public net. (default: false)
         }
     },
     compilers: {
